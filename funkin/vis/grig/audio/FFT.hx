37f1f893f65e04f4599e5fab0003967d9ed4f519
NeeEoo <NeeEoo@users.noreply.github.com>
2024-05-22 23:56:05 +0200

    Small Optimization

--- funkin/vis/grig/audio/FFT.hx
+++ funkin/vis/grig/audio/FFT.hx
@@ -62,7 +62,7 @@ class FFT
 		for (i in 0...reversed.length)
 			reversed[i] = bitReverse(i);
 		for (i in 0...Std.int(n / 2))
-			roots[i] = Complex.exp(new Complex(0, i * (TWO_PI / n)));
+			roots[i] = Complex.expPhi(i * (TWO_PI / n));
 	}
 
 	/*
